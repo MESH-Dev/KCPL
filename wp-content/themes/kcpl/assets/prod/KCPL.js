bbf1132f7b34811df1ba45708856545e17573ebc
Pat Eason <patrick@pateason.com>
2014-07-30 11:23:14 -0400

    framework made according to what Josh wants and started on header

---
+++ wp-content/themes/kcpl/assets/prod/KCPL.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+  //start with the localized object, that's important
+  console.log(KCPL);
+
+  //header functions
+  
+
+  //footer functions
+});
---
+++ wp-content/themes/kcpl/functions.php
@@ -0,0 +1,62 @@
+<?php
+  //enqueue scripts and styles *use production assets. Dev assets are located in assets/css and assets/js
+  function KCPL_scripts() {
+  	wp_enqueue_style( 'KCPL-style', get_stylesheet_uri() );
+  	wp_enqueue_script( 'KCPL-script', get_template_directory_uri().'/assets/prod/KCPL.js', array('jquery'), '1.0.0', true );
+    wp_localize_script( 'KCPL-script', 'KCPL',array(
+      'ajaxurl' => admin_url('admin-ajax.php'),
+      'curUser' => get_current_user_id()
+      )
+    );
+  }
+  add_action( 'wp_enqueue_scripts', 'KCPL_scripts' );
+
+  //theme supports
+  add_theme_support('post-thumbnails');
+  $defaults = array(
+    'flex-height'   => true,
+    'flex-width'    => true,
+    'height'        => 78,
+    'width'         => 194,
+    'default-image' => get_template_directory_uri() . '/assets/img/logo.png',
+    'header-text'   => false
+  );
+  add_theme_support('custom-header', $defaults);
+  add_theme_support('custom-background');
+  add_theme_support('html5');
+  add_theme_support('automatic-feed-links');
+
+  //menus
+  register_nav_menus(array(
+  	'main_nav' => 'Header and breadcrumb trail heirarchy',
+  	'social_nav' => 'Social menu used throughout'
+  ));
+
+  //widgets
+  register_sidebar(array(
+	   'name'          => __( 'Footer - Left' ),
+	   'id'            => 'footer-left',
+	   'description'   => '',
+     'class'         => '',
+	   'before_widget' => '',
+	   'after_widget'  => '',
+	   'before_title'  => '<h2>',
+	   'after_title'   => '</h2>' ));
+
+  //editor style
+  add_editor_style('assets/css/wp-editor.css');
+
+  //login page style
+  function KCPL_loginCSS() {
+	   echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/assets/img/wp-login.css"/>';
+  } add_action('login_head', 'KCPL_loginCSS');
+
+  //footer attribution
+  function KCPL_footer_admin () {
+	   echo 'Theme developed by <a href="http://meshfresh.com">MESH Design</a>.';
+  } add_filter('admin_footer_text', 'KCPL_footer_admin');
+
+  //disable code editors
+  define('DISALLOW_FILE_EDIT', true);
+
+?>
