ab765426c329b6019b412b0775a2b1b9f8f6ab36
Pat Eason <patrick@pateason.com>
2014-08-01 12:58:58 -0400

    heading and paragraphs

--- wp-content/themes/kcpl/index.php
+++ wp-content/themes/kcpl/index.php
@@ -1,22 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-
-    <div class="row">
-        <div style="height: 200px"></div>
-    </div>
-
-    <div class="row">
-        <div class="three columns">
-
-            <!-- Sidebar secondary menu -->
-            <input type="submit" />
-            <button>Create an Account</button>
-
-        </div>
-        <div class="nine columns">
-        </div>
-    </div>
+  index!
 </div>
 
 <?php get_footer(); ?>
