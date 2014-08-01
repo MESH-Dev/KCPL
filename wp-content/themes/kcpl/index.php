3d74028385939a4c044a8d2d08cab7459ec595c6
robbiesh <sherrard.rob@gmail.com>
2014-08-01 17:09:37 -0400

    Info box and short of vertical callouts

--- wp-content/themes/kcpl/index.php
+++ wp-content/themes/kcpl/index.php
@@ -1,7 +1,35 @@
 <?php get_header(); ?>
 
 <div class="container">
-  index!
+
+    <div class="two columns">
+
+        <div style="height: 500px;">
+
+            <div class="KCPL_info-box">
+                <div class="KCPL_info-box-header">
+                    <span class="KCPL_info-box-header-title">Info</span>
+                </div>
+                <div class="KCPL_info-box-body">
+                    <span class="KCPL_info-box-body-title">Title this is the title it's short</span>
+                    <span class="KCPL_info-box-body-date">Date 3, 2014</span>
+                    <span class="KCPL_info-box-body-excerpt">Learn how easy it is to make your own healthy smoothies.</span>
+                    <span class="KCPL_info-box-body-link">More &nbsp; ≈</span>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="two columns">
+
+        <div class="KCPL_short-callout">
+            <span>Short Call Out</span>
+        </div>
+
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
