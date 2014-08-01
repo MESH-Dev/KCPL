6a4a70c5fbafd40966967e25f9ea47b6e7122a10
Pat Eason <patrick@pateason.com>
2014-08-01 15:53:40 -0400

    header viewport and base css for banner menu/breadcrumbs

--- wp-content/themes/kcpl/elements.tpl.php
+++ wp-content/themes/kcpl/elements.tpl.php
@@ -2,6 +2,35 @@
 
 <?php if(have_posts()){while(have_posts()){the_post(); ?>
 
+<div id="banner" class="KCPL_background-red">
+  <div class="container">
+    <div class="gutter">
+      <?php if(has_nav_menu('main_nav')){
+          $defaults = array(
+            'theme_location'  => 'main_nav',
+            'menu'            => 'main_nav',
+            'container'       => false,
+            'container_class' => '',
+            'container_id'    => 'main_navBannerCont',
+            'menu_class'      => 'menu',
+            'menu_id'         => 'main_nav',
+            'echo'            => true,
+            'fallback_cb'     => 'wp_page_menu',
+            'before'          => '',
+            'after'           => '',
+            'link_before'     => '',
+            'link_after'      => '',
+            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'depth'           => 0,
+            'walker'          => ''
+          ); wp_nav_menu( $defaults );
+        }else{
+          echo "<p><em>main_nav</em> doesn't exist! Create it and it'll render here.</p>";
+        } ?>
+    </div>
+  </div>
+</div>
+
 <div id="content">
   <div class="container">
     <div class="columns four alpha" id="contentSecondary">
