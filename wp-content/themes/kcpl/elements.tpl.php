a1c41e4004a75083b51a6503f4c03dd928eb1d08
Pat Eason <patrick@pateason.com>
2014-08-01 17:19:07 -0400

    home banner markup and styling

--- wp-content/themes/kcpl/elements.tpl.php
+++ wp-content/themes/kcpl/elements.tpl.php
@@ -2,31 +2,13 @@
 
 <?php if(have_posts()){while(have_posts()){the_post(); ?>
 
-<div id="banner" class="KCPL_background-red">
+<div id="bannerHome" class="KCPL_background-yellow">
   <div class="container">
-    <div class="gutter">
-      <?php if(has_nav_menu('main_nav')){
-          $defaults = array(
-            'theme_location'  => 'main_nav',
-            'menu'            => 'main_nav',
-            'container'       => 'div',
-            'container_class' => '',
-            'container_id'    => 'main_navBannerCont',
-            'menu_class'      => 'menu clearfix',
-            'menu_id'         => 'main_nav',
-            'echo'            => true,
-            'fallback_cb'     => 'wp_page_menu',
-            'before'          => '',
-            'after'           => '',
-            'link_before'     => '',
-            'link_after'      => '',
-            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            'depth'           => 0,
-            'walker'          => ''
-          ); wp_nav_menu( $defaults );
-        }else{
-          echo "<p><em>main_nav</em> doesn't exist! Create it and it'll render here.</p>";
-        } ?>
+    <div class="columns seven">
+      <div class="gutter">
+        <h1>Your library is more than millions of resources at your fingertips, We are your community center.</h1>
+        <h2>Share your thoughts on our new site!</h2>
+      </div>
     </div>
   </div>
 </div>
