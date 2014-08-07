5fc2d9b9e3d8d8268a95e9b2399b58fe6599fa4c
Pat Eason <patrick@pateason.com>
2014-08-07 16:52:54 -0400

    markup

--- wp-content/themes/kcpl/index.php
+++ wp-content/themes/kcpl/index.php
@@ -13,81 +13,52 @@
    <div class="container">
 
      <div class="row">
-       <div class="columns four alpha clearfix">
+       <div class="columns eight alpha clearfix">
 
-          <div class="KCPL_listing2-cal KCPL_background-blue">
-             <span class="title">Featured Events</span>
+          <div class="KCPL_listing4">
+             <span class="title KCPL_background-red">Curated Collection Listing</span>
              <div class="gutter">
-                <div class="entry">
-                   <div class="left">
-                      <span class="entry-title">Topic Name</span>
+                <div class="row">
+                   <div class="entry">
+                      <div class="image"></div>
+                      <div class="number">1</div>
+                      <div class="content">
+                        <span class="entry-title">Very long three line title this is the long title it is crazy long</span>
+                        <span class="entry-author">Short Author Name</span>
+                      </div>
                    </div>
-                   <div class="right">
-                      <span class="entry-date">April 10, 3PM</span>
-                      <span class="entry-loc">Main Charleston Library</span>
-                      <div class="entry-tags">
-                        <a href="#">Teens</a> ,
-                        <a href="#">Screenings</a> ,
-                        <a href="#">Lectures</a> ,
-                        <a href="#">History</a> ,
-                        <a href="#">Technology</a> ,
-                        <a href="#">Adults</a> ,
-                        <a href="#">Poetry</a>
+                   <div class="entry">
+                      <div class="image"></div>
+                      <div class="number">1</div>
+                      <div class="content">
+                        <span class="entry-title">Very long three line title this is the long title it is crazy long</span>
+                        <span class="entry-author">Short Author Name</span>
                       </div>
                    </div>
                 </div>
-                <div class="entry">
-                   <div class="left">
-                      <span class="entry-title">Feed Your Head Lunchtime Program: I'm a Really Fun-Gi: Mushroom Growing 101</span>
+                <div class="row">
+                   <div class="entry">
+                      <div class="image"></div>
+                      <div class="number">1</div>
+                      <div class="content">
+                        <span class="entry-title">Very long three line title this is the long title it is crazy long</span>
+                        <span class="entry-author">Short Author Name</span>
+                      </div>
                    </div>
-                   <div class="right">
-                      <span class="entry-date">April 10, 3PM</span>
-                      <span class="entry-loc">Main Charleston Library Long Name of location and a lot of details</span>
-                      <div class="entry-tags">
-                        <a href="#">Teens</a> ,
-                        <a href="#">Screenings</a> ,
-                        <a href="#">Lectures</a>
+                   <div class="entry">
+                      <div class="image"></div>
+                      <div class="number">1</div>
+                      <div class="content">
+                        <span class="entry-title">Very long three line title this is the long title it is crazy long</span>
+                        <span class="entry-author">Short Author Name</span>
                       </div>
                    </div>
                 </div>
-                <a href="#" class="KCPL_readmore">More Events ≈</a>
              </div>
           </div>
 
        </div>
-       <div class="columns four clearfix">
 
-         <div class="KCPL_listing2-class KCPL_background-green">
-           <span class="title">Online Classes</span>
-           <div class="gutter">
-               <div class="entry">
-                  <div class="left">
-                     <span class="entry-title">Topic Name</span>
-                  </div>
-                  <div class="right">
-                     <p><strong>Resource</strong><br>
-                        <a href="#">www.url.com</a><br>
-                        <strong>Resource</strong><br>
-                        <a href="#">www.url.com</a><br>
-                        <a href="#">www.url.com</a>
-                     </p>
-                  </div>
-               </div>
-               <div class="entry">
-                  <div class="left">
-                     <span class="entry-title">Really Long Topic Name</span>
-                  </div>
-                  <div class="right">
-                     <p><strong>Resource</strong><br>
-                        <a href="#">www.url.com</a><br>
-                        <a href="#">www.url.com</a></p>
-                  </div>
-               </div>
-               <a href="#" class="KCPL_readmore">More Events ≈</a>
-           </div>
-         </div>
-
-       </div>
      </div>
 
    </div>
