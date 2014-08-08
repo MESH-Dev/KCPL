81578330cccc5b853fde0ad1b4b0a23a807588d7
Pat Eason <patrick@pateason.com>
2014-08-08 12:56:12 -0400

    comment markup

--- wp-content/themes/kcpl/index.php
+++ wp-content/themes/kcpl/index.php
@@ -14,24 +14,43 @@
 
      <div class="row">
 
-       <div class="columns four alpha clearfix">
+       <div class="columns eight alpha clearfix">
 
-          <div class="KCPL_popup KCPL_background-red">
-             <div class="gutter">
-                <span>Log in or sign up to rate materials and get recommendations</span>
-                <a href="#" class="KCPL_readmore">Join now ≈</a>
-             </div>
-          </div>
+         <div class="KCPL_comments">
+            <div id="comments" class="comments-area">
+            	<ol class="comment-list">
+            	  <li class="comment even thread-even depth-1 parent" id="comment-1">
 
-       </div>
+            			<div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                     <div class="comment-content">
+                        <span class="name">User Namename</span>
+                        <span class="date">July 17, 2014 2 PM</span>
+                        <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                        <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                        <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                     </div>
 
-       <div class="columns four clearfix">
+            	    <ol class="children">
 
-         <div class="KCPL_popup KCPL_background-red">
-           <div class="gutter">
-               <span>Log in or sign up to rate materials and get recommendations. Log in or sign up to rate materials and get recommendations. Log in or sign up to rate materials and get recommendations</span>
-           </div>
-         </div>
+            		   <li class="comment byuser comment-author-admin bypostauthor odd alt depth-2" id="comment-2">
+
+                     <!-- comment l2 !-->
+
+                     </li>
+
+                  </ol>
+                 </li>
+
+         	     <li class="comment even thread-odd thread-alt depth-1" id="comment-12">
+
+         		      <!-- comment l1 !-->
+
+         		  </li>
+
+            	</ol>
+
+            </div>
+			</div>
 
        </div>
 
