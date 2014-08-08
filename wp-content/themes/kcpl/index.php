ad6d869c0e6d2e426d232994984fa0c04102cae2
Pat Eason <patrick@pateason.com>
2014-08-08 12:06:53 -0400

    markup and styling

--- wp-content/themes/kcpl/index.php
+++ wp-content/themes/kcpl/index.php
@@ -16,76 +16,21 @@
 
        <div class="columns four alpha clearfix">
 
-          <div class="KCPL_oc-profile">
-             <span class="title KCPL_background-red">My Profile</span>
+          <div class="KCPL_popup KCPL_background-red">
              <div class="gutter">
-                <div class="top clearfix">
-                   <div class="profile-img">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prIMG.png" />
-                   </div>
-                   <div class="profile-social">
-                      <a href="#" class="fa fa-lg fa-facebook facebook">facebook</a>
-                      <a href="#" class="fa fa-lg fa-pinterest pinterest">pinterest</a>
-                      <a href="#" class="fa fa-lg fa-tumblr tumblr">tumblr</a>
-                      <a href="#" class="fa fa-lg fa-instagram instagram">instagram</a>
-                      <a href="#" class="fa fa-lg fa-twitter twitter">twitter</a>
-                      <a href="#" class="fa fa-lg fa-google-plus googleplus">googleplus</a>
-                   </div>
-                </div>
-                <div class="bottom clearfix">
-                   <div class="profile-username">
-                      <span class="title">User Name</span>
-                      <span>Location, WV</span>
-                      <a href="#">www.personalsite.com</a>
-                   </div>
-                   <div class="profile-interests">
-                      <span class="title">Interests</span>
-                      <a href="#">Mystery</a> , <a href="#">Romance</a> , <a href="#">Painting</a> , <a href="#">Crafts</a> , <a href="#">Zombies</a>
-                   </div>
-                   <div class="profile-reading">
-                      <span class="title">Currently Reading</span>
-                      <a href="#">Title of This Book by Author McAuthorson</a>
-                   </div>
-                </div>
+                <span>Log in or sign up to rate materials and get recommendations</span>
+                <a href="#" class="KCPL_readmore">Join now ≈</a>
              </div>
           </div>
 
        </div>
 
-       <div class="columns eight omega clearfix">
+       <div class="columns four clearfix">
 
-         <div class="KCPL_oc-profile clearfix">
-            <span class="title KCPL_background-red">My Profile</span>
-            <div class="gutter clearfix">
-               <div class="left">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prIMG.png" />
-               </div>
-               <div class="center">
-                  <div class="profile-username">
-                     <span class="title">User Name</span>
-                     <span>Location, WV</span>
-                     <a href="#">www.personalsite.com</a>
-                  </div>
-                  <div class="profile-interests">
-                     <span class="title">Interests</span>
-                     <a href="#">Mystery</a> , <a href="#">Romance</a> , <a href="#">Painting</a> , <a href="#">Crafts</a> , <a href="#">Zombies</a>
-                  </div>
-               </div>
-               <div class="right">
-                  <div class="profile-social">
-                     <a href="#" class="fa fa-lg fa-facebook facebook">facebook</a>
-                     <a href="#" class="fa fa-lg fa-pinterest pinterest">pinterest</a>
-                     <a href="#" class="fa fa-lg fa-tumblr tumblr">tumblr</a>
-                     <a href="#" class="fa fa-lg fa-instagram instagram">instagram</a>
-                     <a href="#" class="fa fa-lg fa-twitter twitter">twitter</a>
-                     <a href="#" class="fa fa-lg fa-google-plus googleplus">googleplus</a>
-                  </div>
-                  <div class="profile-reading">
-                     <span class="title">Currently Reading</span>
-                     <a href="#">Title of This Book by Author McAuthorson</a>
-                  </div>
-               </div>
-            </div>
+         <div class="KCPL_popup KCPL_background-red">
+           <div class="gutter">
+               <span>Log in or sign up to rate materials and get recommendations. Log in or sign up to rate materials and get recommendations. Log in or sign up to rate materials and get recommendations</span>
+           </div>
          </div>
 
        </div>
