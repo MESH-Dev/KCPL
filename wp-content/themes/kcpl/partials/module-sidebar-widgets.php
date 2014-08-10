b6bc5fac0753bb14625aa704b33e85fa0c1d6e72
Pat Eason <patrick@pateason.com>
2014-08-10 11:24:47 -0400

    sidebar horizontal callout single/multi links

--- wp-content/themes/kcpl/partials/module-sidebar-widgets.php
+++ wp-content/themes/kcpl/partials/module-sidebar-widgets.php
@@ -2,21 +2,43 @@
 $sidebar = KCPL_get_sidebar($pID);
 
 foreach($sidebar as $widget){
+  //horizontal callout - single
   if($widget['field_type'] == 'horizontal-callout-single'){ ?>
 
-    <div class="KCPL_horz-single KCPL_background-<?php echo $widget['field_color']; ?>">
-      <div class="gutter">
-         <span><?php echo $widget['horizontal_callout-single_title']; ?></span>
-      </div>
-    </div>
+    <?php if($widget['field_link'] != ''){ ?>
+      <a target="_blank" href="<?php echo $widget['field_link']; ?>">
+    <?php } ?>
 
-  <?php }elseif($widget['field_type'] == 'horizontal-callout-multi'){ ?>
+        <div class="KCPL_horz-single KCPL_background-<?php echo $widget['field_color']; ?>">
+          <div class="gutter">
+             <span><?php echo $widget['horizontal_callout-single_title']; ?></span>
+          </div>
+        </div>
 
-    <div class="KCPL_horz-multi KCPL_background-<?php echo $widget['field_color']; ?>">
-      <div class="gutter">
-         <span><?php echo $widget['horizontal_callout-multi_title']; ?></span>
+    <?php if($widget['field_link'] != ''){ ?>
+      </a>
+    <?php } ?>
+
+
+  <?php }elseif($widget['field_type'] == 'horizontal-callout-multi'){
+    //horizontal callout - multi ?>
+
+    <?php if($widget['field_link'] != ''){ ?>
+      <a target="_blank" href="<?php echo $widget['field_link']; ?>">
+    <?php } ?>
+
+      <div class="KCPL_horz-multi KCPL_background-<?php echo $widget['field_color']; ?>">
+        <div class="gutter">
+           <span><?php echo $widget['horizontal_callout-multi_title']; ?></span>
+           <?php if($widget['field_link'] != ''){ ?>
+             <a class="KCPL_readmore" href="<?php echo $widget['field_link']; ?>">Read More ≈</a>
+           <?php } ?>
+        </div>
       </div>
-    </div>
+
+    <?php if($widget['field_link'] != ''){ ?>
+      </a>
+    <?php } ?>
 
   <?php }else{
     echo "Hasn't been configured yet. Deal with it.";
