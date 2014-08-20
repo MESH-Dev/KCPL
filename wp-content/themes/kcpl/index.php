9f5bb61070b08a101b36fd372b46124a0656643b
robbiesh <sherrard.rob@gmail.com>
2014-08-20 17:16:18 -0400

    markup to scss

--- wp-content/themes/kcpl/index.php
+++ wp-content/themes/kcpl/index.php
@@ -12,10 +12,133 @@
 <div id="content">
    <div class="container">
 
-        <a class="previous page-numbers" href="http://sa.bkfk-t5yk.accessdomain.com/rickhouse-blog/page/2/"><i class="fa fa-angle-double-left"></i></a>
-        <span class='page-numbers current'>1</span>
-        <a class='page-numbers' href='http://sa.bkfk-t5yk.accessdomain.com/rickhouse-blog/page/2/'>2</a>
-        <a class="next page-numbers" href="http://sa.bkfk-t5yk.accessdomain.com/rickhouse-blog/page/2/"><i class="fa fa-angle-double-right"></i></a>
+     <div class="row">
+
+       <div class="columns four alpha clearfix">
+
+         <div class="KCPL_comments clearfix">
+            <div id="comments" class="comments-area clearfix">
+               <ol class="comment-list clearfix">
+                 <li class="comment even thread-even depth-1 parent clearfix" id="comment-1">
+
+                     <i class="caret"></i>
+                     <div class="comment-wrap clearfix">
+                        <div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                        <div class="comment-content">
+                           <span class="name">User Namename</span>
+                           <span class="date">July 17, 2014 2 PM</span>
+                           <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                           <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                           <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                        </div>
+                     </div>
+
+                   <ol class="children clearfix">
+
+                     <li class="comment byuser comment-author-admin bypostauthor odd alt depth-2 clearfix" id="comment-2">
+                        <i class="caret"></i>
+                        <div class="comment-wrap clearfix">
+                           <div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                           <div class="comment-content">
+                              <span class="name">User Namename</span>
+                              <span class="date">July 17, 2014 2 PM</span>
+                              <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                              <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                              <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                           </div>
+                        </div>
+
+                     </li>
+
+                  </ol>
+                 </li>
+
+                 <li class="comment even thread-odd thread-alt depth-1 clearfix" id="comment-12">
+
+                     <i class="caret"></i>
+                     <div class="comment-wrap clearfix">
+                        <div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                        <div class="comment-content">
+                           <span class="name">User Namename</span>
+                           <span class="date">July 17, 2014 2 PM</span>
+                           <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                           <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                           <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                        </div>
+                     </div>
+
+                 </li>
+
+               </ol>
+
+            </div>
+         </div>
+
+       </div>
+
+       <div class="columns eight omega clearfix">
+
+         <div class="KCPL_comments clearfix">
+            <div id="comments" class="comments-area clearfix">
+               <ol class="comment-list clearfix">
+                 <li class="comment even thread-even depth-1 parent clearfix" id="comment-1">
+
+                     <i class="caret"></i>
+                     <div class="comment-wrap clearfix">
+                        <div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                        <div class="comment-content">
+                           <span class="name">User Namename</span>
+                           <span class="date">July 17, 2014 2 PM</span>
+                           <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                           <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                           <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                        </div>
+                     </div>
+
+                   <ol class="children clearfix">
+
+                     <li class="comment byuser comment-author-admin bypostauthor odd alt depth-2 clearfix" id="comment-2">
+                        <i class="caret"></i>
+                        <div class="comment-wrap clearfix">
+                           <div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                           <div class="comment-content">
+                              <span class="name">User Namename</span>
+                              <span class="date">July 17, 2014 2 PM</span>
+                              <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                              <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                              <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                           </div>
+                        </div>
+
+                     </li>
+
+                  </ol>
+                 </li>
+
+                 <li class="comment even thread-odd thread-alt depth-1 clearfix" id="comment-12">
+
+                     <i class="caret"></i>
+                     <div class="comment-wrap clearfix">
+                        <div class="comment-img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/phIMG.png" /></div>
+                        <div class="comment-content">
+                           <span class="name">User Namename</span>
+                           <span class="date">July 17, 2014 2 PM</span>
+                           <div class="comment-comment">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod dictum gravida. Nullam semper dui elit, non fermentum consequat a dui. Etiam suscipit nibh at consequat gravida.</div>
+                           <a class="reply KCPL_readmore" href="#">Reply ≈</a>
+                           <a class="leave-comment KCPL_readmore" href="#">Leave a Comment ≈</a>
+                        </div>
+                     </div>
+
+                 </li>
+
+               </ol>
+
+            </div>
+         </div>
+
+       </div>
+
+     </div>
 
    </div>
 </div>
