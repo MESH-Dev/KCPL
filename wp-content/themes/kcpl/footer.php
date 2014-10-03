79d225a920ebb22fd6159726906f688393dd4131
Pat Eason <patrick@pateason.com>
2014-10-03 11:15:32 -0400

    breadcrumbs are jacked, yo

--- wp-content/themes/kcpl/footer.php
+++ wp-content/themes/kcpl/footer.php
@@ -1,57 +1,58 @@
-<footer>
-  <div class="container">
-    <div class="gutter">
-      <div class="row">
-        <div class="columns two alpha">
-          <?php dynamic_sidebar('footer-col1'); ?>
-        </div>
-        <div class="columns two">
-          <?php dynamic_sidebar('footer-col2'); ?>
-        </div>
-        <div class="columns two">
-          <?php dynamic_sidebar('footer-col3'); ?>
-        </div>
-        <div class="columns two">
-          <?php dynamic_sidebar('footer-col4'); ?>
-        </div>
-        <div class="columns three offset-by-one omega">
-          <div>
-            <span class="title">Reach Out</span>
-            <?php if(has_nav_menu('social_nav')){
-                $defaults = array(
-                  'theme_location'  => 'social_nav',
-                  'menu'            => 'social_nav',
-                  'container'       => 'div',
-                  'container_class' => 'clearfix',
-                  'container_id'    => 'social_navCont',
-                  'menu_class'      => 'menu',
-                  'menu_id'         => 'social_nav',
-                  'echo'            => true,
-                  'fallback_cb'     => 'wp_page_menu',
-                  'before'          => '',
-                  'after'           => '',
-                  'link_before'     => '',
-                  'link_after'      => '',
-                  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                  'depth'           => 0,
-                  'walker'          => ''
-                ); wp_nav_menu( $defaults );
-              }else{
-                echo "<p><em>social_nav</em> doesn't exist! Create it and it'll render here.</p>";
-              } ?>
+  <footer>
+    <div class="container">
+      <div class="gutter">
+        <div class="row">
+          <div class="columns two alpha">
+            <?php dynamic_sidebar('footer-col1'); ?>
           </div>
-          <div>
-            <span class="title">Donate</span>
-            <div id="footer-donate">
-                <span>Please support the annual fund</span>
-                <a href="#">Make a Donation</a>
+          <div class="columns two">
+            <?php dynamic_sidebar('footer-col2'); ?>
+          </div>
+          <div class="columns two">
+            <?php dynamic_sidebar('footer-col3'); ?>
+          </div>
+          <div class="columns two">
+            <?php dynamic_sidebar('footer-col4'); ?>
+          </div>
+          <div class="columns three offset-by-one omega">
+            <div>
+              <span class="title">Reach Out</span>
+              <?php if(has_nav_menu('social_nav')){
+                  $defaults = array(
+                    'theme_location'  => 'social_nav',
+                    'menu'            => 'social_nav',
+                    'container'       => 'div',
+                    'container_class' => 'clearfix',
+                    'container_id'    => 'social_navCont',
+                    'menu_class'      => 'menu',
+                    'menu_id'         => 'social_nav',
+                    'echo'            => true,
+                    'fallback_cb'     => 'wp_page_menu',
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                    'depth'           => 0,
+                    'walker'          => ''
+                  ); wp_nav_menu( $defaults );
+                }else{
+                  echo "<p><em>social_nav</em> doesn't exist! Create it and it'll render here.</p>";
+                } ?>
+            </div>
+            <div>
+              <span class="title">Donate</span>
+              <div id="footer-donate">
+                  <span>Please support the annual fund</span>
+                  <a href="#">Make a Donation</a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</footer>
+  </footer>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/kcpl/header.php
+++ wp-content/themes/kcpl/header.php
@@ -7,80 +7,91 @@
   <?php wp_head(); ?>
 </head>
 <body <?php if(wp_is_mobile()){$mobileClass="KCPLmobile";}else{$mobileClass="KCPLmobile";} body_class($mobileClass);?>>
-<header>
-  <div id="KCPL_header-search">
-     <div class="container">
-        <?php get_search_form(); ?>
-     </div>
+
+<?php //ONLY show this for mobile browsers
+  if(wp_is_mobile()){ ?>
+
+  <div id="mobileWrap">
+    
   </div>
-  <div id="header-util">
-    <div class="container clearfix">
-      <div id="header-logo">
-        <a href="<?php echo home_url(); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a>
-      </div>
-      <div class="gutter clearfix">
-        <?php if(has_nav_menu('util_nav')){
-            $defaults = array(
-              'theme_location'  => 'util_nav',
-              'menu'            => 'util_nav',
-              'container'       => false,
-              'container_class' => '',
-              'container_id'    => 'util_navCont',
-              'menu_class'      => 'menu',
-              'menu_id'         => 'util_nav',
-              'echo'            => true,
-              'fallback_cb'     => 'wp_page_menu',
-              'before'          => '',
-              'after'           => '',
-              'link_before'     => '',
-              'link_after'      => '',
-              'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-              'depth'           => 0,
-              'walker'          => ''
-            ); wp_nav_menu( $defaults );
-          }else{
-            echo "<p><em>util_nav</em> doesn't exist! Create it and it'll render here.</p>";
-          } ?>
-          <div id="header-extra">
-            <div id="header-search">
-              <b></b><span>Search</span>
+
+} ?>
+
+<div id="contentWrap">
+  <header>
+    <div id="KCPL_header-search">
+       <div class="container">
+          <?php get_search_form(); ?>
+       </div>
+    </div>
+    <div id="header-util">
+      <div class="container clearfix">
+        <div id="header-logo">
+          <a href="<?php echo home_url(); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a>
+        </div>
+        <div class="gutter clearfix">
+          <?php if(has_nav_menu('util_nav')){
+              $defaults = array(
+                'theme_location'  => 'util_nav',
+                'menu'            => 'util_nav',
+                'container'       => false,
+                'container_class' => '',
+                'container_id'    => 'util_navCont',
+                'menu_class'      => 'menu',
+                'menu_id'         => 'util_nav',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu',
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
+                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth'           => 0,
+                'walker'          => ''
+              ); wp_nav_menu( $defaults );
+            }else{
+              echo "<p><em>util_nav</em> doesn't exist! Create it and it'll render here.</p>";
+            } ?>
+            <div id="header-extra">
+              <div id="header-search">
+                <b></b><span>Search</span>
+              </div>
+              <div id="header-newsletter">
+                <b></b><span>Newsletter</span>
+              </div>
+              <div id="header-login">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/login.png"/>
+              </div>
             </div>
-            <div id="header-newsletter">
-              <b></b><span>Newsletter</span>
-            </div>
-            <div id="header-login">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/login.png"/>
-            </div>
-          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div id="header-nav">
-    <div class="container clearfix">
-      <div class="gutter clearfix">
-        <?php if(has_nav_menu('main_nav')){
-            $defaults = array(
-            	'theme_location'  => 'main_nav',
-            	'menu'            => 'main_nav',
-            	'container'       => false,
-            	'container_class' => '',
-            	'container_id'    => 'main_navCont',
-            	'menu_class'      => 'menu',
-            	'menu_id'         => 'main_nav',
-            	'echo'            => true,
-            	'fallback_cb'     => 'wp_page_menu',
-            	'before'          => '',
-            	'after'           => '',
-            	'link_before'     => '',
-            	'link_after'      => '',
-            	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            	'depth'           => 0,
-            	'walker'          => ''
-            ); wp_nav_menu( $defaults );
-          }else{
-            echo "<p><em>main_nav</em> doesn't exist! Create it and it'll render here.</p>";
-          } ?>
+    <div id="header-nav">
+      <div class="container clearfix">
+        <div class="gutter clearfix">
+          <?php if(has_nav_menu('main_nav')){
+              $defaults = array(
+              	'theme_location'  => 'main_nav',
+              	'menu'            => 'main_nav',
+              	'container'       => false,
+              	'container_class' => '',
+              	'container_id'    => 'main_navCont',
+              	'menu_class'      => 'menu',
+              	'menu_id'         => 'main_nav',
+              	'echo'            => true,
+              	'fallback_cb'     => 'wp_page_menu',
+              	'before'          => '',
+              	'after'           => '',
+              	'link_before'     => '',
+              	'link_after'      => '',
+              	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+              	'depth'           => 0,
+              	'walker'          => ''
+              ); wp_nav_menu( $defaults );
+            }else{
+              echo "<p><em>main_nav</em> doesn't exist! Create it and it'll render here.</p>";
+            } ?>
+        </div>
       </div>
     </div>
-  </div>
-</header>
+  </header>
