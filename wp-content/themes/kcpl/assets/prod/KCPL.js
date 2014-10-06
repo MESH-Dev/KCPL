dabdf212af0b98722dbba82955ea3ba0db187003
Pat Eason <patrick@pateason.com>
2014-10-06 14:21:36 -0400

    cookie retrieval function in JS

--- wp-content/themes/kcpl/assets/prod/KCPL.js
+++ wp-content/themes/kcpl/assets/prod/KCPL.js
@@ -2,6 +2,21 @@ jQuery(document).ready(function($){
   //global functions
   try{Typekit.load();}catch(e){}
 
+
+  //cookie & auth functions
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+  }
+  console.log(getCookie('ezproxy'));
+
+
   //header functions
   $('#header-search').click(function(){
      $('#KCPL_header-search').toggleClass('active');
