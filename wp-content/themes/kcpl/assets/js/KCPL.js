9c43db789f0dfc58b58c5a62bc288e51cc674f64
Pat Eason <patrick@pateason.com>
2014-10-07 14:32:51 -0400

    thanks Obama

--- wp-content/themes/kcpl/assets/js/KCPL.js
+++ wp-content/themes/kcpl/assets/js/KCPL.js
@@ -155,4 +155,43 @@ jQuery(document).ready(function($){
     }
   });
 
+
+  //cookies
+    console.log('Aloha! Ohana!');
+
+    var kcplidC = getCookie('kcplID');
+    var ezproxyC = getCookie('ezproxy');
+
+    console.log(kcplidC);
+    console.log(ezproxyC);
+
+    var jqForm = $('form.left');
+
+    $('form.left').submit(function(e){
+      e.preventDefault();
+      var uid = $(this).find('input[name="user"]').val();
+      setCookie('kcplID',uid,365);
+      var ourcookie = getCookie('kcplID');
+      $(this).unbind('submit').submit();
+    });
+
+    function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
 });
