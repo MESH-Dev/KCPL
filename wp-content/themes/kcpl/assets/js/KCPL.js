79812f2e498871eaf27ba5ea8acd50a6f0182d8a
Pat Eason <patrick@pateason.com>
2014-10-07 14:37:57 -0400

    test

--- wp-content/themes/kcpl/assets/js/KCPL.js
+++ wp-content/themes/kcpl/assets/js/KCPL.js
@@ -179,7 +179,7 @@ jQuery(document).ready(function($){
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+    document.cookie = cname + "=" + cvalue + ";domain=.ezproxy.kanawhalibrary.org " + expires;
 }
 
 function getCookie(cname) {
