dc14484d2ea52094a68261e2842102550f1e30ca
Pat Eason <patrick@pateason.com>
2014-10-07 14:16:07 -0400

    more source for auth test

--- auth/auth.js
+++ auth/auth.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Aloha');
+
+    var ourForm = getElementById('#loginForm');
+    var jqForm = $('form.left');
+    console.log(ourForm);
+    console.log(jqform);
+
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
