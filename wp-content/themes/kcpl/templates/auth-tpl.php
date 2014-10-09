d45d223e30540dcd30ca324c35c6e58a1c0492dc
Pat Eason <patrick@pateason.com>
2014-10-09 13:52:52 -0400

    login functions and restricted admin bar

--- wp-content/themes/kcpl/templates/auth-tpl.php
+++ wp-content/themes/kcpl/templates/auth-tpl.php
@@ -17,21 +17,27 @@ $color = get_field('section_color',$pID);
     <?php include_once(locate_template('partials/module-sidebar-nav.php')); ?>
 
     <div class="columns eight omega" id="contentPrimary">
+      <h2>Welcome to the Online Community</h2>
+      <p>Your Online Community and your Library logins are separate. If this is your first time logging in, use your library card and PIN, then you can change your password later on if you want to.</p>
+      <?php
+      $options = get_site_option('kcpl-oc');
+      $args = array(
+        'echo'           => true,
+        'redirect'       => get_permalink($options['dashboard']),
+        'form_id'        => 'loginform',
+        'label_username' => __( 'Card Number' ),
+        'label_password' => __( 'PIN Number' ),
+        'label_log_in'   => __( 'Log In' ),
+        'id_username'    => 'user_login',
+        'id_password'    => 'user_pass',
+        'id_remember'    => 'rememberme',
+        'id_submit'      => 'wp-submit',
+        'remember'       => false,
+        'value_username' => NULL,
+        'value_remember' => false
+      ); ?>
+      <?php wp_login_form( $args ); ?>
 
-      <div id="authContainer" class="active">
-        <div id="authContainer-load">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/loading.gif" />
-          <h5>The gears are turning and we're authenticating you with the Online Community.</h5>
-        </div>
-        <div id="authContainer-success">
-
-        </div>
-        <div id="authContainer-fail">
-          <h5>Something went horribly wrong and we couldn't authenticate you with the Online Community.</h5>
-        </div>
-      </div>
-
-    </div>
 
   </div>
 </div>
